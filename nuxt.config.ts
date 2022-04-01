a26c94b2d5714f2625a72d8a8fff4e33810e7ea6
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-04-02 08:13:48 +0900

    supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/supabase'
+  ],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY
+  },
   css: [
     '@/assets/scss/reset.scss'
   ]
